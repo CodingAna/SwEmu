152e283293e98dafe71ec155efff70879de4dae7
Ana <74157379+CodingAna@users.noreply.github.com>
2023-05-02 17:33:16 +0200

    Fixed .roundedRect

--- CustomDraw.js
+++ CustomDraw.js
@@ -32,27 +32,78 @@ export class CustomDraw {
     let p3 = new Point(p2.x, p1.y);
     let p4 = new Point(p1.x, p2.y);
 
-
     let dy14 = p4.y - p1.y; // It must be guaranteed that p4.y >= p1.y
-    let r = (dy14 / 2) * roundness;
+    let dx13 = p3.x - p1.x; // p3.x >= p1.x
+    let r = (MyMath.min(dy14, dx13) / 2) * roundness;
 
     let pm1 = new Point(p1.x+r, p1.y+r);
     let pm2 = new Point(p2.x-r, p2.y-r);
     let pm3 = new Point(p3.x-r, p3.y+r);
     let pm4 = new Point(p4.x+r, p4.y-r);
 
-    this._context.beginPath();
-    this._context.arc(pm1.x, pm1.y, r, Math.PI, Math.PI * 2);
-    this._context.arc(pm2.x, pm2.y, r, 0, Math.PI);
-    this._context.arc(pm3.x, pm3.y, r, Math.PI, Math.PI * 2);
-    this._context.arc(pm4.x, pm4.y, r, 0, Math.PI);
+    let pm1u = new Point(pm1.x, p1.y);;
+    let pm1l = new Point(p1.x, pm1.y);
 
-    if (fill) this._context.fill();
-    else this._context.stroke();
+    let pm3u = new Point(pm3.x, p3.y);
+    let pm3r = new Point(p3.x, pm3.y);
 
-    this.rect(new Point(pm1.x, p1.y), new Point(pm2.x, p2.y));
-    this.rect(new Point(p1.x, pm1.y), new Point(p2.x, pm2.y));
+    let pm2d = new Point(pm2.x, p2.y);
+    let pm2r = new Point(p2.x, pm2.y);
+
+    let pm4d = new Point(pm4.x, p4.y);
+    let pm4l = new Point(p4.x, pm4.y);
+
+    // Top Left Arc
+    // TODO: Only do loop if fill
+    this.arc(pm1, r, fill, 1*Math.PI, 1.5*Math.PI);
+    if (fill) {
+      for (let i=0; i<r; i+=1/4) {
+        let pm1ir = pm1u.interpolate(pm1, i/r);
+        let pm1id = pm1l.interpolate(pm1, i/r);
+        this.line(pm1ir, pm1id);
+      }
+    } else this.line(pm1u, pm3u);
+
+    // Top Right Arc
+    // TODO: Only do loop if fill
+    this.arc(pm3, r, fill, 1.5*Math.PI, 2*Math.PI);
+    if (fill) {
+      for (let i=0; i<r; i+=1/4) {
+        let pm3il = pm3u.interpolate(pm3, i/r);
+        let pm3id = pm3r.interpolate(pm3, i/r);
+        this.line(pm3il, pm3id);
+      }
+    } else this.line(pm3r, pm2r)
+
+    // Bottom Right Arc
+    // TODO: Only do loop if fill
+    this.arc(pm2, r, fill, 0, 0.5*Math.PI);
+    if (fill) {
+      for (let i=0; i<r; i+=1/4) {
+        let pm2il = pm2d.interpolate(pm2, i/r);
+        let pm2iu = pm2r.interpolate(pm2, i/r);
+        this.line(pm2il, pm2iu);
+      }
+    } else this.line(pm2d, pm4d);
+
+    // Bottom Left Arc
+    // TODO: Only do loop if fill
+    this.arc(pm4, r, fill, 0.5*Math.PI, Math.PI);
+    if (fill) {
+      for (let i=0; i<r; i+=1/4) {
+        let pm4ir = pm4d.interpolate(pm4, i/r);
+        let pm4iu = pm4l.interpolate(pm4, i/r);
+        this.line(pm4ir, pm4iu);
+      }
+    } else this.line(pm4l, pm1l);
+
+    if (fill) {
+      this.rect(pm1u, pm2d);
+      this.rect(pm1l, pm2r);
+    }
   }
+
+  // TODO: triangle = (p1, p2, p3, ...) => {...}
 
   arc = (p, r, fill, start, around) => {
     if (!(p instanceof Point)) return;
