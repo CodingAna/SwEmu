4def90e5a9832a0faef16de675e632139c5d4734
Ana <74157379+CodingAna@users.noreply.github.com>
2023-05-05 23:29:30 +0200

    Added fillStart and fillEnd functions

--- Utils.js
+++ Utils.js
@@ -2,4 +2,15 @@ export let Utils = {
   startsWith: (string, start) => {
     return string.substring(0, start.length) === start;
   },
+  fillStart: (string, char, length) => {
+    let out = "";
+    for (let i=0; i<length-string.length; i++) out += char;
+    out += string;
+    return out;
+  },
+  fillEnd: (string, char, length) => {
+    let out = string;
+    for (let i=0; i<length-string.length; i++) out += char;
+    return out;
+  },
 }
